48164bf7f1a2c9f0e6025d43fbc2aac900445b6e
Megan Simmons <m24simmons@gmail>
2020-07-08 12:05:34 -0500

    Initial Commit

---
+++ I am me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am me
+//
+//  Created by Student on 7/8/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
